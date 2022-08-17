ed60ee42ba7a874bb22448ee46eb3e3c1079faaa
vasilyev <vasilyev@mps.mpg.de>
2022-08-17 16:53:53 +0200

    Refactoring a class FlareLocalization

--- src/new/flare_localization.py
+++ src/new/flare_localization.py
@@ -12,25 +12,15 @@ class FlareTimeIsNotWithinTheTimeRangeInTheTargetPixelFile_Exception(Exception):
 
 class FlareLocalization:
     def __init__(self, flare_time: float, tpf: TargetPixelFile):
-        self._check_that_flare_time_value_is_within_the_time_array_from_tpf(flare_time=flare_time,
-                                                                            time_array=tpf.time.value)
+        self._check_that_flare_time_value_is_within_the_time_array_from_tpf(flare_time=flare_time,  time_array=tpf.time.value)
         self.flare_time = flare_time
         self.tpf = tpf
-        self.tpf_image_at_flare_time = self._get_tpf_image_at_flare_time(self.flare_time, self.tpf.time.value,
-                                                                         self.tpf.flux.value)
+        self.tpf_image_at_flare_time = None
         self.flare_image: np.ndarray = None
 
-    @staticmethod
     def _check_that_flare_time_value_is_within_the_time_array_from_tpf(self, flare_time: float, time_array: np.ndarray):
         if (flare_time < np.min(time_array)) or (flare_time > np.max(time_array)):
             raise FlareTimeIsNotWithinTheTimeRangeInTheTargetPixelFile_Exception
-
-    def get_flare_image(self):
-        return self.flare_image
-
-    def _get_tpf_image_at_flare_time(self, flare_time: float, tpf_time: np.ndarray, tpf_images: np.ndarray) -> np.ndarray:
-        index = self._find_image_index(flare_time=flare_time, time_array=tpf_time)
-        return tpf_images[index]
 
     def _find_image_index(self,  flare_time: float, time_array: np.ndarray) -> int:
         index_flare = np.squeeze(np.argmin(np.abs(time_array - flare_time)))
@@ -39,6 +29,26 @@ class FlareLocalization:
                   f"from the target pixel file! Take index corresponding to the nearest value {time_array[index_flare]}")
         return index_flare
 
+    def find_tpf_image_at_flare_time(self) -> "FlareLocalization":
+        index = self._find_image_index(flare_time=self.flare_time, time_array=self.tpf_time)
+        self.tpf_image_at_flare_time = self.tpf.flux.tpf_images[index]
+        return self
+
+    def get_flare_image(self, detrending_method: str = "polynom"):
+        """detrend flare"""
+        nx = self.tpf_image_at_flare_time.shape[0]
+        ny = self.tpf_image_at_flare_time.shape[1]
+
+
+        return self.flare_image
+
+    def get_image_shape(self) -> [int, int]:
+
+
+
+    def localize_flare(self):
+        """ """
+        return self
 
 
 
